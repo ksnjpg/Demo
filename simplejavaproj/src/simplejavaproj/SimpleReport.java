d8f6e5ca607e529e66b7e3c0413391451a1991f3
Suresh Naidu <Suresh Naidu@192.168.2.4>
2017-08-11 09:03:56 -0700

    method body added

--- simplejavaproj/src/simplejavaproj/SimpleReport.java
+++ simplejavaproj/src/simplejavaproj/SimpleReport.java
@@ -1,11 +1,11 @@
 package simplejavaproj;
 
+import org.openqa.selenium.WebDriver;
+
 public class SimpleReport 
 {
  WebDriver driver;
- public static void test1();
- {
- 
- }
+ public static void test1() {
+	 System.out.println("hi");}
  
 }
